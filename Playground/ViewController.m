25f822b7ddf2e47410099aefe8d1a5e472a0a5db
HenryFan <henry.fan@escapex.com>
2018-07-24 17:41:13 +0800

    Pod install RXCollection and Implement Map, Filter and Fold functional methods

--- Playground/ViewController.m
+++ Playground/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import <RXCollections/RXCollection.h>
 
 @interface ViewController ()
 
@@ -14,16 +15,42 @@
 
 @implementation ViewController
 
+#pragma mark - Lifecycle
+
+- (void)dealloc {
+
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+
+    NSArray *array = @[@(1), @(2), @(3)];
+
+    // Map
+    NSArray *mappedArray = [array rx_mapWithBlock:^id(id each) {
+        return @(pow([each integerValue], 2));
+    }];
+
+    // Filter
+    NSArray *filteredArray = [array rx_filterWithBlock:^BOOL(id each) {
+        return ([each integerValue] % 2 == 0);
+    }];
+
+    // Fold
+    NSNumber *sum = [array rx_foldWithBlock:^id(id memo, id each) {
+        return @([memo integerValue] + [each integerValue]);
+    }];
+
+    [[array rx_mapWithBlock:^id(id each) {
+        return [each stringValue];
+    }] rx_foldInitialValue:@"" block:^id(id memo, id each) {
+        return [memo stringByAppendingString:each];
+    }];
 }
 
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
-
 
 @end
